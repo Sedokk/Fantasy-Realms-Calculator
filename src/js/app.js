25e72c7bb5fc94cc2b271b291a9d05d24908a414
Gleb <st090625@gmail.com>
2022-05-17 22:39:16 +0300

    added function of taking card in the hand

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,46 @@
 
 
 const handNode = document.querySelector('.hand__card-wrapper');
-
 const cardBtns = document.querySelectorAll('.cards__card');
+const cardsAmount = document.querySelector('.hand__cards-amount');
 
 
 function onCardClick(e) {
+
+    if (isSevenCards()) {
+
+        createCard(e)
+
+    }
     
+    cardsAmount.innerText = handNode.childNodes.length;
+
+    console.log(handNode.childNodes.length);
+}
+
+function isSevenCards() {
+    return handNode.childNodes.length < 7
+}
+
+function createCard(e) {
     const name = e.target.innerText.replace(/\d/g, '')
     const value = e.target.innerText.replace(/\D/g, '')
 
-    console.log(name, value);
+    const handBlock = document.createElement('div')
+    const handName = document.createElement('span')
+    const handValue = document.createElement('span')
+
+    handName.innerText = name;
+    handValue.innerText = value;
+
+    handBlock.classList.add('hand__card')
+    handName.classList.add('hand__card-name')
+    handValue.classList.add('hand__card-power')
+
+    handBlock.prepend(handName)
+    handBlock.append(handValue)
+
+    handNode.prepend(handBlock)
 }
 
 
