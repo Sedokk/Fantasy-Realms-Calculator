4150a090f1c6c8b77a16d871c903a369e2e983ed
Gleb <st090625@gmail.com>
2022-05-24 22:05:50 +0300

    prepared hand

--- dist/js/cards.js
+++ dist/js/cards.js
@@ -9,19 +9,126 @@ const cards = [
         suit: 'army',
         power: 5,
         blanked: false,
-        action: ['increasing for each', 'unblanking'],
+        action: ['inc each', 'unbl'],
         names: {
             increasing: ['land'],
             unblanking: ['army'],
         },
-        exeptions: {},
+        exeptions: [],
         number: {
             increasing: 10,
             decreasing: 0,
             extra: 0,
         },
-    }
+    },
+    {
+        name: 'basilisk',
+        suit: 'beast',
+        power: 35,
+        blanked: false,
+        action: ['bl'],
+        names: {
+            increasing: [],
+            unblanking: [],
+            blanking: ['army', 'leader', 'beast']
+        },
+        exeptions: ['basilisk'],
+        number: {
+            increasing: 0,
+            decreasing: 0,
+            extra: 0,
+        },
+    },
+    {
+        name: 'great flood',
+        suit: 'flood',
+        power: 32,
+        blanked: false,
+        action: ['bl'],
+        names: {
+            increasing: [],
+            unblanking: [],
+            blanking: ['army', 'land', 'flame']
+        },
+        exeptions: ['mountain', 'lightning'],
+        number: {
+            increasing: 0,
+            decreasing: 0,
+            extra: 0,
+        },
+    },
+    {
+        name: 'lightning',
+        suit: 'flame',
+        power: 11,
+        blanked: false,
+        action: ['inc pres'],
+        names: {
+            increasing: ['rainstorm'],
+            unblanking: [],
+        },
+        exeptions: [],
+        number: {
+            increasing: 30,
+            decreasing: 0,
+            extra: 0,
+        },
+    },
+    {
+        name: 'rainstorm',
+        suit: 'weather',
+        power: 8,
+        blanked: false,
+        action: ['inc each', 'bl'],
+        names: {
+            increasing: ['flood'],
+            blanking: ['flame'],
+        },
+        exeptions: ['lightning'],
+        number: {
+            increasing: 10,
+            decreasing: 0,
+            extra: 0,
+        },
+    },
+    {
+        name: 'warship',
+        suit: 'weapon',
+        power: 23,
+        blanked: false,
+        action: ['bl self', 'unb'],
+        names: {
+            blanking: ['flood'],
+            unblanking: ['hz'],
+        },
+        exeptions: [],
+        number: {
+            increasing: 0,
+            decreasing: 0,
+            extra: 0,
+        },
+    },
+    {
+        name: 'war dirigible',
+        suit: 'weapon',
+        power: 35,
+        blanked: false,
+        action: ['bl self'],
+        names: {
+            increasing: ['land'],
+            unblanking: ['army'],
+        },
+        exeptions: [],
+        number: {
+            increasing: 10,
+            decreasing: 0,
+            extra: 0,
+        },
+    },
+    
 ]
+
+
 
 
 
