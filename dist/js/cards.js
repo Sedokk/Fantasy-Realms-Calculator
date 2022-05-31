0a6258d15c781fa4a1e11e0b2cf4832925262ade
Gleb <st090625@gmail.com>
2022-05-31 23:42:50 +0300

    added some cards

--- dist/js/cards.js
+++ dist/js/cards.js
@@ -121,6 +121,127 @@ const cards = [
             extra: 0,
         },
     },
+    {
+        name: 'ranger',
+        suit: 'army',
+        power: 5,
+        blanked: false,
+        action: ['inc each', 'pardon'],
+        names: {
+            increasing: ['land'],
+            pardoning: ['army'],
+        },
+        exeptions: [],
+        number: {
+            increasing: 10,
+            decreasing: 0,
+            extra: 0,
+        },
+    },
+    {
+        name: 'fores',
+        suit: 'land',
+        power: 7,
+        blanked: false,
+        action: ['inc each'],
+        names: {
+            increasing: ['beast', 'elven archers'],
+            unblanking: [],
+            blanking: ['army', 'leader', 'beast']
+        },
+        exeptions: ['basilisk'],
+        number: {
+            increasing: 0,
+            decreasing: 0,
+            extra: 0,
+        },
+    },
+    {
+        name: 'elven archer',
+        suit: 'army',
+        power: 10,
+        blanked: false,
+        action: ['inc abs'],
+        names: {
+            increasing: ['weather'],
+            unblanking: [],
+            blanking: []
+        },
+        exeptions: [],
+        number: {
+            increasing: 5,
+            decreasing: 0,
+            extra: 0,
+        },
+    },
+    {
+        name: 'lightnin',
+        suit: 'flame',
+        power: 11,
+        blanked: false,
+        action: ['inc pres'],
+        names: {
+            increasing: ['rainstorm'],
+            unblanking: [],
+        },
+        exeptions: [],
+        number: {
+            increasing: 30,
+            decreasing: 0,
+            extra: 0,
+        },
+    },
+    {
+        name: 'rainstor',
+        suit: 'weather',
+        power: 8,
+        blanked: false,
+        action: ['inc each', 'bl'],
+        names: {
+            increasing: ['flood'],
+            blanking: ['flame'],
+        },
+        exeptions: ['lightning'],
+        number: {
+            increasing: 10,
+            decreasing: 0,
+            extra: 0,
+        },
+    },
+    {
+        name: 'candl',
+        suit: 'flame',
+        power: 2,
+        blanked: false,
+        action: ['inc pres all'],
+        names: {
+            increasing: ['book of changes', 'bell tower', 'wizard']
+        },
+        exeptions: [],
+        number: {
+            increasing: 100,
+            decreasing: 0,
+            extra: 0,
+        },
+    },
+    {
+        name: 'elven longbo',
+        suit: 'weapon',
+        power: 3,
+        blanked: false,
+        action: ['inc pres'],
+        names: {
+            increasing: ['elven archers', 'warlord', 'beastmaster'],
+            unblanking: [],
+            blanking: []
+        },
+        exeptions: [],
+        number: {
+            increasing: 30,
+            decreasing: 0,
+            extra: 0,
+        },
+    },
     
 ]
 
