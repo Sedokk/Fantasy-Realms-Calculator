9266372bc64e3627daab1cd07f68fd40e67829d7
Gleb <st090625@gmail.com>
2022-06-29 22:04:40 +0300

    added necromancer func

--- src/js/createCards.js
+++ src/js/createCards.js
@@ -2,9 +2,20 @@ import cards from './cards.js'
 
 let hand;
 const handNames = [];
+const cardsAllowedNode = document.querySelector('.hand__cards-allowed')
 
 function onClick(e) {
-    if (handNames.length > 6) return
+
+    let allowedAmount;
+    if (e.target.dataset.name === 'necromancer' || handNames.includes('necromancer')) {
+        allowedAmount = 7;
+        cardsAllowedNode.innerText = 8;
+    } else {
+        allowedAmount = 6;
+        cardsAllowedNode.innerText = 7;
+    }
+
+    if (handNames.length > allowedAmount) return
 
     let name;
     
